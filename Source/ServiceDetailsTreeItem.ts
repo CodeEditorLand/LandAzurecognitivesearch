39fdef167965114ca36ed5645645f34821916479
Nikola Hristov <nikola@nikolahristov.tech>
2023-10-30 12:05:57 +0200

    squash!

--- Source/ServiceDetailsTreeItem.ts
+++ Source/ServiceDetailsTreeItem.ts
@@ -8,35 +8,44 @@ import { IDocumentRepository } from "./IDocumentRepository";
 import { SearchService } from "azure-arm-search/lib/models";
 import { getResourcesPath } from "./constants";
 import { Uri } from "vscode";
-import * as path from 'path';
+import * as path from "path";
 
-export class ServiceDetailsTreeItem extends AzureTreeItem implements IDocumentRepository {
-    public readonly commandId: string = "azureCognitiveSearch.openDocument";
-    public readonly contextValue: string = "azureCognitiveSearchServiceDetails";
-    public readonly label: string = "Service Details";
-    public readonly namePrefix: string;
-    readonly itemName: string;
-    readonly itemKind: string = "service";
-    readonly extension: string = "azssvc";
+export class ServiceDetailsTreeItem
+	extends AzureTreeItem
+	implements IDocumentRepository
+{
+	public readonly commandId: string = "azureCognitiveSearch.openDocument";
+	public readonly contextValue: string = "azureCognitiveSearchServiceDetails";
+	public readonly label: string = "Service Details";
+	public readonly namePrefix: string;
+	readonly itemName: string;
+	readonly itemKind: string = "service";
+	readonly extension: string = "azssvc";
 
-    public constructor(
-        parent: AzureParentTreeItem,
-        private readonly searchService: SearchService) {
-        super(parent);
-        this.itemName = searchService.name || "";
-        this.namePrefix = `service-${searchService.name}`;
-    }
+	public constructor(
+		parent: AzureParentTreeItem,
+		private readonly searchService: SearchService
+	) {
+		super(parent);
+		this.itemName = searchService.name || "";
+		this.namePrefix = `service-${searchService.name}`;
+	}
 
-    public iconPath: { light: string | Uri; dark: string | Uri } = {
-        light: path.join(getResourcesPath(), 'light', 'info.svg'),
-        dark: path.join(getResourcesPath(), 'dark', 'info.svg')
-    };
+	public iconPath: { light: string | Uri; dark: string | Uri } = {
+		light: path.join(getResourcesPath(), "light", "info.svg"),
+		dark: path.join(getResourcesPath(), "dark", "info.svg"),
+	};
 
-    async readContent(): Promise<{ content: any; etag?: string | undefined; } | undefined> {
-        return { content: this.searchService };
-    }
+	async readContent(): Promise<
+		{ content: any; etag?: string | undefined } | undefined
+	> {
+		return { content: this.searchService };
+	}
 
-    async updateContent(content: any, etag?: string | undefined): Promise<void> {
-        throw new Error("Updating service details not supported.");
-    }
+	async updateContent(
+		content: any,
+		etag?: string | undefined
+	): Promise<void> {
+		throw new Error("Updating service details not supported.");
+	}
 }
